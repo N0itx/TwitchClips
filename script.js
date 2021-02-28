93a44d9c0feb0235eb1521ee126e357ac4b1d5f3
ShiroLB <45373591+N0itx@users.noreply.github.com>
2021-02-27 22:30:38 -0600

    Clip Download Option
    Ahora se puede descargar el clip si se selecciona la opción de descarga ya que no toma el clip desde el reproductor de Twitch, se toma el link directo del video.
    Aunque esto puede generar errores al obtener el link exacto ya que twitch cambia los links seguido, se ocupa tener en mantenimiento

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 //===== COPY TO CLIPBOARD =====//
 
-// $(document).ready(function() {
-//   $("div#slug").click(function() {
-//     // alert("Text: " + $(this).text())
-//     copyToClipboard(this)
-//       // $(this).hide();
-//     $(this).css({
-//       color: '#70ffa9'
-//     });
-//     // $(this).animate({
-//     //     fontSize: '.9em'
-//     // });
-//   });
-// });
-
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
@@ -22,17 +8,15 @@ function copyToClipboard(element) {
   $temp.remove();
 }
 
-
 //===== CALL TWITCH API =====//
 
-// First AJAX call for searching twitch.tv channels
+// AJAX call for searching twitch.tv channels
 $(document).ready(function() {
   $("#submit_search #user_search").click(function() {
-    // $('#submit_search').on('submit', function() {
     var channel = document.getElementById('username').value;
     var tiempo = document.getElementById('tiempo').value;
     var cantidad = document.getElementById('cantidad').value;
-    var userURL = `https://api.twitch.tv/kraken/clips/top?channel=${channel.toLowerCase()}&period=${tiempo}&trending=true&limit=${cantidad}`;
+    var userURL = `https://api.twitch.tv/kraken/clips/top?channel=${channel.toLowerCase()}&period=${tiempo}&trending=false&limit=${cantidad}`;
     $.ajax({
       url: userURL,
       type: 'GET',
@@ -43,28 +27,21 @@ $(document).ready(function() {
         'Client-ID': 'njdjdkepu3gygen6rwc9lxwaio4082'
       },
       success: function(data) {
-        // Multiple search without refreshing page
-        // Have this before you call cloneElement() so it doesn't conflict
-        // var clean = $('#clean').clone();
-        // $('#clean').on('hidden.bs.modal', function() {
-        //   $(this).remove();
-        //   var clone = clean.clone();
-        //   $('body').append(clone);
-        //   $('#clean').hide();
-        // });
+        
         if (data.clips.length == 0) {
-          $("div#api").html(`Acceso denegado: [ no es posible obtener los clips de este canal ]`);
-          abort()
+          $("div#api").html(`0 Clips detectados en [ ${channel} / ${tiempo} / false / ${cantidad} ]`);
+          return
         } else {
-          $("div#api").text(`Streamer: ${channel} | Clips: ${data.clips.length} | Periodo: ${tiempo} | Trending: true`);
+          $("div#api").text(`Streamer: ${channel} | Clips: ${data.clips.length} | Periodo: ${tiempo} | Trending: false`);
         }
 
         cloneElement(data);
-        // $('.list-group-item:first').hide()
-        console.log(data);
+        
+        // console.log(data);
+        console.log(`Clips ${data.clips.length}`);
 
         $("div #streamer-info").append(`
-          <img src="${data.clips[0].broadcaster.logo}" alt="logo noitx">
+          <img src="${data.clips[0].broadcaster.logo}" alt="broadcaster.logo">
           <h2>${data.clips[0].broadcaster.display_name}</h2>
           <p>UserID: ${data.clips[0].broadcaster.id}</p>
           <a href="${data.clips[0].broadcaster.channel_url}">${data.clips[0].broadcaster.channel_url}</a>
@@ -74,7 +51,7 @@ $(document).ready(function() {
         function cloneElement(data) {
           for (var i = 0; i < data.clips.length; i++) {
 
-            let titulo = data.clips[i].title.substring(0,20);
+            let titulo = data.clips[i].title.substring(0, 20);
             let categoria = data.clips[i].game;
             let duracion = data.clips[i].duration;
             let views = data.clips[i].views;
@@ -82,12 +59,50 @@ $(document).ready(function() {
             let url = data.clips[i].url.split("?");
             let autor = data.clips[i].curator.display_name;
 
-            $("div #lista-clips").append(`
-              <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
-              <iframe data-src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io" frameborder="0" scrolling="no" autoplay="true" preload="metadata" allowfullscreen="true" src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io"></iframe>
-              <br><div id="slug" class="info">${url[0]}</div>
-              <br><div class="info">${autor}</div>
-              <div class="separador pto"></div>`);
+            if (document.getElementById("dl-check").checked) {
+              let id = data.clips[i].tracking_id;
+              let make = data.clips[i].thumbnails.medium;
+              // let broad_id = data.clips[i].broadcast_id;
+              let video = '';
+
+              //LOS LINKS QUE USA TWITCH SUELEN ACTUALIZARSE Y POR ESO SE OCUPA ADAPTAR EL LINK
+              if (make.includes('vod-')) {
+                //console.log('vod-')
+                let spt = make.split("/");
+                let clip_id = spt[3].split("-");
+                video = `https://${spt[2]}/vod-${clip_id[1]}-offset-${clip_id[3]}.mp4`;
+              } else if (make.includes('offset')) {
+                // console.log('offset')
+                let spt = make.split("/");
+                let clip_id = spt[3].split("-");
+                video = `https://${spt[2]}/${clip_id[0]}-offset-${clip_id[2]}.mp4`;
+              } else if (make.includes('AT-cm')) {
+                // console.log('AT-cm')
+                let spt = make.split("/");
+                let clip_id = spt[3].split("-");
+                video = `https://${spt[2]}/AT-${clip_id[1]}.mp4`;
+              } else {
+                console.log(make)
+              }
+
+              $("div #lista-clips").append(`
+                <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+                <video width="720" height="440" controls>
+                    <source src="${video}" type="video/mp4">
+                </video>
+                <br><div id="slug" class="info">${url[0]}</div>
+                <br><div class="info">${autor}</div>
+                <div class="separador pto"></div>
+              `);
+            } else {
+              $("div #lista-clips").append(`
+                <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+                <iframe data-src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io" frameborder="0" scrolling="no" autoplay="true" preload="metadata" allowfullscreen="true" src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io"></iframe>
+                <br><div id="slug" class="info">${url[0]}</div>
+                <br><div class="info">${autor}</div>
+                <div class="separador pto"></div>
+              `);
+            }
           }
         }
         $("div#slug").click(function() {
@@ -95,6 +110,21 @@ $(document).ready(function() {
           $(this).css({
             color: '#70ffa9'
           });
+        });
+        $(document).on("click", "#user_search", function() {
+          $(".row").remove();
+          $("#contenido").append(`
+            <div class="row">
+                <div id="lista-clips" class="column-left">
+                    <!-- CLIPS -->
+                </div>
+                <div class="column-right">
+                    <div id="streamer-info" class="desc">
+                        <!-- Broadcaster Info -->
+                    </div>
+                </div>
+            </div>
+          `);
         });
       }
     });
