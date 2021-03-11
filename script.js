de75cb2a1926dcbe53e25b5c3dd4dff0fe47311c
ShiroLB <45373591+N0itx@users.noreply.github.com>
2021-03-11 00:09:06 -0600

    la rompi

--- script.js
+++ script.js
@@ -1,111 +1,24 @@
-const client = new tmi.Client({
-  options: {
-    debug: false,
-    messagesLogLevel: "info"
-  },
-  connection: {
-    reconnect: true,
-    secure: true
-  },
-  identity: {
-    username: 'zhokro',
-    password: 'oauth:upzcq1esih74udaj29c04vw1tj4g9r'
-  },
-  channels: ['zhokro']
-});
+//===== COPY TO CLIPBOARD =====//
 
-let join_to = [
-  'soypan', 'nuvia_ouo', 'xxkeivicxx', 'alicejuliana', 'caprimint',
-  'elded', 'iaaras2', 'conterstine', 'juansguarnizo', 'xcry',
-  'n0itx', 'twitch', 'rubius', 'drdianaa', 'cateVinelle',
-  'catsen'
-  /*, 'sapharic', 'sacriel', 'Sparkles_qt', 'inochifantasy',
-    'scrubing', 'missgigglesss', 'nakkida', 'andy', 'fpskrystal',
-    'landfork', 'asleepypeach', 'donnaybunny', 'pictobeam', 'teepee',
-    'nozomi', 'chun', 'diffizzle', 'leonie', 'brionuhh',
-    'kohrean', 'bobbypoffgaming', 'chillbobagginz', 'aylaahmao',
-    'spekel', 'gooderness', 'jessekiri', 'lillyvinnily',
-    'lloulou', 'choi', 'nuubix', 'maimy', 'galaxyaus', 'mizkif',
-    'brookeab', 'ciriell', 'neeko', 'JoeWatermelon', 'lilypichu',
-    'kephrii', 'inkspirate', 'admiralbahroo', 'syndicate',
-    'tsikyo', 'dreamt', 'niisa', 'neko', 'forgottenproject',
-    'nikatine', 'jinja', 'smexi', 'imtedious', 'august',
-    'spofie', 'sacredxo', 'leyla', 'jaee', 'tenz',
-    'umbra', 'pointcrow', 'tfue', 'summit1g', 'bugha', 'dubs', 'bizzle',
-    'imnio'*/
-];
-
-function conectar() {
-  client.connect().catch(console.error);
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
 }
 
-function desconectar() {
-  client.disconnect().catch(console.error)
-}
+//===== CALL TWITCH API =====//
 
-// client.connect().catch(console.error);
-
-client.on('connected', connection);
-client.on('disconnected', disconnection);
-client.on('message', chat_messages);
-
-function disconnection(reason) {
-  console.log(`* Disconnction by: ${reason}`);
-  $('.connection').css('textShadow', '#ff0000 0 0 10px');
-  $('.connection').text(`DISCONECTED`);
-}
-
-function connection(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-  $('.connection').css('textShadow', '#3fff00 0 0 10px');
-  $('.connection').text(`ONLINE`);
-
-  JoinTo();
-}
-
-function JoinTo() {
-  let time = 0;
-  const pta = setInterval(() => {
-    client.join(join_to[time])
-      .then((data) => {
-        join_Cannels(data[0].replace('#', ''))
-      });
-    time++;
-    if (time == join_to.length) {
-      clearInterval(pta)
-    }
-  }, 1000);
-}
-
-async function join_Cannels(nick) {
-  const resp = await API_User(nick);
-  card_JL(resp, true)
-
-}
-
-//===== HTML CARDS =====//
-
-function card_JL(nick, join) {
-  let tipo = join ? 'join' : 'left';
-  let date = new Date();
-  $("#JL-channel").prepend(`
-    <div class="JL">
-      <img src="${nick.logo}" alt="">
-      <div class="info ${tipo}">
-        <span class="nick">${nick.display_name}</span>
-        <span class="hora">${date.getHours()}:${date.getMinutes()}</span>
-      </div>
-    </div>
-  `);
-  removeExedent('JL', 15);
-}
-
-//===== TWITCH API V5 =====//
-
-function API_User(nick) {
-  return new Promise(resolve => {
+//===== TOP CLIPS BY CHANNEL =====//
+$(document).ready(function() {
+  $("#submit_search #user_search").click(function() {
+    var channel = document.getElementById('username').value;
+    var tiempo = document.getElementById('tiempo').value;
+    var cantidad = document.getElementById('cantidad').value;
+    var userURL = `https://api.twitch.tv/kraken/clips/top?channel=${channel.toLowerCase()}&period=${tiempo}&trending=false&limit=${cantidad}`;
     $.ajax({
-      url: `https://api.twitch.tv/kraken/users?login=${nick}`,
+      url: userURL,
       type: 'GET',
       dataType: 'json',
       contentType: 'application/json',
@@ -114,61 +27,196 @@ function API_User(nick) {
         'Client-ID': 'njdjdkepu3gygen6rwc9lxwaio4082'
       },
       success: function(data) {
-        $.ajax({
-          url: `https://api.twitch.tv/kraken/channels/${data.users[0]._id}`,
-          type: 'GET',
-          dataType: 'json',
-          contentType: 'application/json',
-          headers: {
-            'Accept': 'application/vnd.twitchtv.v5+json',
-            'Client-ID': 'njdjdkepu3gygen6rwc9lxwaio4082'
-          },
-          success: function(data2) {
-            resolve(data2);
+
+        if (data.clips.length == 0) {
+          $("div#api").html(`0 Clips detectados en [ ${channel} / ${tiempo} / false / ${cantidad} ]`);
+          return
+        } else {
+          $("div#api").text(`Streamer: ${channel} | Clips: ${data.clips.length} | Periodo: ${tiempo} | Trending: false`);
+        }
+
+        // console.log(data);
+        // return
+
+        cloneElement(data);
+
+        $("div #streamer-info").append(`
+          <img src="${data.clips[0].broadcaster.logo}" alt="broadcaster.logo">
+          <h2>${data.clips[0].broadcaster.display_name}</h2>
+          <p>UserID: ${data.clips[0].broadcaster.id}</p>
+          <a href="${data.clips[0].broadcaster.channel_url}">${data.clips[0].broadcaster.channel_url}</a>
+          `);
+
+        function cloneElement(data) {
+          for (var i = 0; i < data.clips.length; i++) {
+
+            let titulo = data.clips[i].title.substring(0, 20);
+            let categoria = data.clips[i].game;
+            let duracion = data.clips[i].duration;
+            let views = data.clips[i].views;
+            let slug = data.clips[i].slug;
+            let url = data.clips[i].url.split("?");
+            let autor = data.clips[i].curator.display_name;
+
+            if (document.getElementById("dl-check").checked) {
+              let id = data.clips[i].tracking_id;
+
+              let video = data.clips[i].thumbnails.medium.replace("-preview-480x272.jpg", ".mp4")
+
+              $("div #lista-clips").append(`
+                <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+                <video width="720" height="440" controls>
+                    <source src="${video}" type="video/mp4">
+                </video>
+                <br><div id="slug" class="info">${url[0]}</div>
+                <br><div class="info">${autor}</div>
+                <div class="separador pto"></div>
+              `);
+            } else {
+              $("div #lista-clips").append(`
+                <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+                <iframe data-src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io" frameborder="0" scrolling="no" autoplay="true" preload="metadata" allowfullscreen="true" src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io"></iframe>
+                <br><div id="slug" class="info">${url[0]}</div>
+                <br><div class="info">${autor}</div>
+                <div class="separador pto"></div>
+              `);
+            }
           }
+        }
+        //===== COPY TO CLIPBOARD =====//
+        $("div#slug").click(function() {
+          copyToClipboard(this)
+          $(this).css({
+            color: '#70ffa9'
+          });
+          $(this).text(`LINK COPIADO!`)
         });
       }
     });
+    //===== BORRA LOS CLIPS ANTERIORES Y GENERA NUEVOS =====//
+    $(document).on("click", "#user_search", function() {
+      $(".row").remove();
+      $("#contenido").append(`
+        <div class="row">
+          <div id="lista-clips" class="column-left">
+          </div>
+          <div class="column-right">
+            <div id="streamer-info" class="desc">
+            </div>
+          </div>
+        </div>
+      `);
+    });
   });
-}
+});
 
-const ignore_nick = 'nightbot,streamelements,xkvbotx';
 
-//===== CHAT =====//
-function chat_messages(chs, usr, msg, self) {
-  const username = usr['username'];
-  const nick = usr['display-name'];
-  chs = chs.replace('#', '')
+//===== GET CLIP INFO FROM SLUG =====//
+$(document).ready(function() {
+  $("#submit_search #clip_search").click(function() {
+    var slug = document.getElementById('clip_url').value;
 
-  if (ignore_nick.includes(username)) {
-    // console.log(`${username} detectado`)
-    return;
+    /*
+    https://clips.twitch.tv/PeppyStylishSaladPanicBasket--mRZFM54EU9cdzTX
+    https://www.twitch.tv/soypan/clip/PeppyStylishSaladPanicBasket--mRZFM54EU9cdzTX?filter=clips&range=24hr&sort=time
+    */
+    if (slug.includes('twitch.tv')) {
+      slug = slug.split('/')[slug.includes('clips.') ? 3 : 5];
+      if (slug.includes('?')) { slug = slug.split('?')[0] }
+    }
+
+    var userURL = `https://api.twitch.tv/kraken/clips/${slug}`;
+    $.ajax({
+      url: userURL,
+      type: 'GET',
+      dataType: 'json',
+      contentType: 'application/json',
+      headers: {
+        'Accept': 'application/vnd.twitchtv.v5+json',
+        'Client-ID': 'njdjdkepu3gygen6rwc9lxwaio4082'
+      },
+      success: function(data) {
+
+        // console.log(data);
+        // return
+        
+        $("div#api").html(`Streamer: ${data.broadcaster.display_name} | Autor: ${data.curator.display_name} | Fecha: ${data.created_at.split('T')[0]} | VOD: <a href="${data.vod.url}">${data.vod.url}</a>`)
+
+        $("div #streamer-info").append(`
+          <img src="${data.broadcaster.logo}" alt="broadcaster.logo">
+          <h2>${data.broadcaster.display_name}</h2>
+          <p>UserID: ${data.broadcaster.id}</p>
+          <a href="${data.broadcaster.channel_url}">${data.broadcaster.channel_url}</a>
+        `);
+
+        let titulo = data.title.substring(0, 20);
+        let categoria = data.game;
+        let duracion = data.duration;
+        let views = data.views;
+        let slug = data.slug;
+        let url = data.url.split("?");
+        let autor = data.curator.display_name;
+
+        if (document.getElementById("dl-check").checked) {
+          //TOMA LA URL DEL VIDEO TRANSFORMANDO LA URL DE LA PREVIEW
+          let video = data.thumbnails.medium.replace("-preview-480x272.jpg", ".mp4")
+
+          $("div #lista-clips").append(`
+            <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+            <video width="720" height="440" controls>
+                <source src="${video}" type="video/mp4">
+            </video>
+            <br><div id="slug" class="info">${url[0]}</div>
+          `);
+        } else {
+          $("div #lista-clips").append(`
+            <div id="info">Titulo: <b>${titulo}...</b> | Categoria: <b>${categoria}</b> | Duracion: <b>${duracion}s</b> | Views: <b>${views}</b></div><br>
+            <iframe data-src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io" frameborder="0" scrolling="no" autoplay="true" preload="metadata" allowfullscreen="true" src="https://clips.twitch.tv/embed?clip=${slug}&amp;parent=n0itx.github.io"></iframe>
+            <br><div id="slug" class="info">${url[0]}</div>
+          `);
+        }
+
+        //===== COPY TO CLIPBOARD =====//
+        $("div#slug").click(function() {
+          copyToClipboard(this)
+          $(this).css({
+            color: '#70ffa9'
+          });
+          $(this).text(`LINK COPIADO!`)
+        });
+      },
+      error: function(data) {
+        $("div#api").text(`No se detecto ningun clip, asegurate que el link que proporcionaste sea el correcto`);
+      }
+    });
+    $(document).on("click", "#clip_search", function() {
+      $(".row").remove();
+      $("#contenido").append(`
+        <div class="row">
+          <div id="lista-clips" class="column-left">
+          </div>
+          <div class="column-right">
+            <div id="streamer-info" class="desc">
+            </div>
+          </div>
+        </div>
+      `);
+    });
+  });
+});
+
+//===== API OPTION TABS =====//
+function optionClips(evt, s_option) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  // console.log(`${chs} - ${nick}: ${msg}`)
-  // $("#chat").prepend(`
-  //   <p class="mensaje">${chs} - ${nick}: ${msg}</p>
-  // `);
-  message_card(nick, msg);
-  removeExedent('mensaje', 15);
-}
-//https://static-cdn.jtvnw.net/emoticons/v2/881956/default/light/1.0
-
-function message_card (nick, msg) {
-  if (msg.includes('LUL')) {
-    $(this).css(
-      'background-color', '#8500ff'
-    );
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  $("#chat").prepend(`
-    <p class="mensaje">${nick}: ${msg}</p>
-  `);
+  document.getElementById(s_option).style.display = "block";
+  evt.currentTarget.className += " active";
 }
-
-
-//===== EXTRAS =====//
-
-function removeExedent(name, cantidad) {
-  const elements = document.getElementsByClassName(name);
-  if (elements.length == cantidad + 1)
-    elements[cantidad].parentNode.removeChild(elements[cantidad]);
-}
+document.getElementById("defaultOpen").click();
